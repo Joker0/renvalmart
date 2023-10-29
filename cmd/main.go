c671d11c10f18117b3c9d9f666081a45638e0a53
Asfar <asfar.triyadi@gmail.com>
2023-10-29 22:26:40 +0700

    chore: tidy main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -5,29 +5,49 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/joker0/renvalmart/config"
+	"github.com/joker0/renvalmart/database"
+	_ "github.com/joker0/renvalmart/docs"
 	"github.com/joker0/renvalmart/internal/app/models"
 	"github.com/joker0/renvalmart/internal/app/routes"
+	"github.com/joker0/renvalmart/seeds"
 	"github.com/labstack/echo/v4"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
 	// Initialize Echo framework
 	e := echo.New()
 
+	// e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// e.Logger.Fatal(e.Start(":1323"))
+
 	// Load application configurations
-	config, err := LoadConfig() // Implement your configuration loading function
+	config, err := config.LoadConfig() // Implement your configuration loading function
 	if err != nil {
 		log.Fatalf("Error loading configuration: %v", err)
 	}
 
 	// Initialize the database connection
-	db, err := InitializeDatabase(config)
+	db, err := database.InitializeDatabase(config)
 	if err != nil {
 		log.Fatalf("Error initializing the database: %v", err)
+	}
+
+	// Migrate the database
+	database.Up(db)
+
+	var itemCount int64
+	db.Model(&models.Item{}).Count(&itemCount)
+
+	if itemCount != 0 {
+		database.Down(db)
+	} else {
+		// Seed data for the Item, Supplier, and User tables
+		seeds.SeedItems(db)
+		seeds.SeedSuppliers(db)
+		seeds.SeedUsers(db)
+		seeds.SeedStocks(db)
 	}
 
 	// Set up middleware
@@ -47,63 +67,4 @@ func main() {
 
 	serverAddr := fmt.Sprintf(":%s", port)
 	e.Logger.Fatal(e.Start(serverAddr))
-}
-
-func LoadConfig() (config.DatabaseConfig, error) {
-	var config config.DatabaseConfig
-
-	// Load environment variables from the .env file
-	if err := godotenv.Load(); err != nil {
-		return config, fmt.Errorf("Error loading .env file: %v", err)
-	}
-
-	// Read configuration from environment variables
-	config.DBPort = os.Getenv("DB_Port")
-	config.DBHost = os.Getenv("DB_Host")
-	config.DBUsername = os.Getenv("DB_Username")
-	config.DBPassword = os.Getenv("DB_Password")
-	config.DBName = os.Getenv("DB_Name")
-
-	// If configuration values are missing, you can set defaults or return an error
-	if config.DBHost == "" {
-		return config, fmt.Errorf("DB_Host is not set in environment variables")
-	}
-
-	if config.DBPort == "" {
-		config.DBPort = "5432" // Default PostgreSQL port
-	}
-
-	if config.DBUsername == "" {
-		return config, fmt.Errorf("DB_Username is not set in environment variables")
-	}
-
-	if config.DBPassword == "" {
-		return config, fmt.Errorf("DB_Password is not set in environment variables")
-	}
-
-	if config.DBName == "" {
-		return config, fmt.Errorf("DB_Name is not set in environment variables")
-	}
-
-	return config, nil
-}
-
-func InitializeDatabase(config config.DatabaseConfig) (*gorm.DB, error) {
-	// Initialize the database connection
-	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Shanghai",
-		config.DBHost,
-		config.DBPort,
-		config.DBUsername,
-		config.DBPassword,
-		config.DBName)
-	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
-
-	if err != nil {
-		return nil, err
-	}
-
-	// Migrate the database schemas (run database migrations here)
-	db.AutoMigrate(&models.Item{}, &models.Stock{}, &models.Supplier{}) // Uncomment and replace with your model
-
-	return db, nil
 }
--- config/config.go
+++ config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type DatabaseConfig struct {
 	DBHost     string
 	DBPort     string
@@ -7,4 +14,43 @@ type DatabaseConfig struct {
 	DBPassword string
 	DBName     string
 	// Add other database configuration options here
+}
+
+func LoadConfig() (DatabaseConfig, error) {
+	var config DatabaseConfig
+
+	// Load environment variables from the .env file
+	if err := godotenv.Load(); err != nil {
+		return config, fmt.Errorf("Error loading .env file: %v", err)
+	}
+
+	// Read configuration from environment variables
+	config.DBPort = os.Getenv("DB_Port")
+	config.DBHost = os.Getenv("DB_Host")
+	config.DBUsername = os.Getenv("DB_Username")
+	config.DBPassword = os.Getenv("DB_Password")
+	config.DBName = os.Getenv("DB_Name")
+
+	// If configuration values are missing, you can set defaults or return an error
+	if config.DBHost == "" {
+		return config, fmt.Errorf("DB_Host is not set in environment variables")
+	}
+
+	if config.DBPort == "" {
+		config.DBPort = "5432" // Default PostgreSQL port
+	}
+
+	if config.DBUsername == "" {
+		return config, fmt.Errorf("DB_Username is not set in environment variables")
+	}
+
+	if config.DBPassword == "" {
+		return config, fmt.Errorf("DB_Password is not set in environment variables")
+	}
+
+	if config.DBName == "" {
+		return config, fmt.Errorf("DB_Name is not set in environment variables")
+	}
+
+	return config, nil
 }
