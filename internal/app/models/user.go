42687a3bfa7855991204cca9392c34206f662356
Asfar <asfar.triyadi@gmail.com>
2023-10-30 18:29:10 +0700

    fix user password not stored in db and add userresponse model

--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -7,13 +7,20 @@ import (
 // User represents the user model in the database
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Password string `json:"-"`
-	Email    string `json:"email"`
+	Name     string `json:"name" gorm:"Not null"`
+	Password string `json:"password"`
+	Email    string `json:"email" gorm:"unique"`
 	Role     string `json:"role"`
 }
 
 type LoginRequest struct {
 	Username string `json:"username"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type UserResponse struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
 }
